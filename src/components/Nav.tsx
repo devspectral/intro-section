44ae52787ab6242e3a0c957bf5266883f6fc3be8
unknown <devspectral@gmail.com>
2022-10-04 13:10:31 -0400

    added transition to hover

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,16 +9,16 @@ function Nav() {
                     <nav>
                         <ul className='flex p-4 items-center justify-center '>
                             <li className='mr-8 font-bold text-4xl mb-1'>snap</li>
-                            <li className='mx-6 text-[#696969] hover:text-[#141414]'>Features</li>
-                            <li className='mx-6 text-[#696969] hover:text-[#141414]'>Company</li>
-                            <li className='mx-6 text-[#696969] hover:text-[#141414]'>Careers</li>
-                            <li className='mx-6 text-[#696969] hover:text-[#141414]'>About</li>                          
+                            <li className='mx-6 text-[#696969] hover:text-[#141414] transition'>Features</li>
+                            <li className='mx-6 text-[#696969] hover:text-[#141414] transition'>Company</li>
+                            <li className='mx-6 text-[#696969] hover:text-[#141414] transition'>Careers</li>
+                            <li className='mx-6 text-[#696969] hover:text-[#141414] transition'>About</li>                          
                        </ul>
                     </nav>
                 </div>
                 <div className='flex justify-center items-center'>
-                    <button className='p-2 text-[#696969] hover:text-[#141414] w-[158px] h-[60px]'>Login</button>
-                    <button className='p-2 text-[#696969] hover:text-[#141414] w-[158px] h-[60px] border-2 rounded-3xl border-[#696969] hover:border-[#141414] '>Register</button>
+                    <button className='p-2 text-[#696969] hover:text-[#141414] w-[158px] h-[60px] transition'>Login</button>
+                    <button className='p-2 text-[#696969] hover:text-[#141414] w-[158px] h-[60px] border-2 rounded-3xl border-[#696969] hover:border-[#141414] transition delay-75'>Register</button>
                 </div>
             </div>
         </div>
